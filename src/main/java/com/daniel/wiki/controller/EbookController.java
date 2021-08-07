69ccd19cfdda78e73146e812b309ba2ed5b239ae
Xinwei Lu <490589474@qq.com>
2021-08-07 17:00:10 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/daniel/wiki/controller/EbookController.java
@@ -0,0 +1,23 @@
+package com.daniel.wiki.controller;
+
+import com.daniel.wiki.domain.Ebook;
+import com.daniel.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/daniel/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.daniel.wiki.service;
+
+import com.daniel.wiki.domain.Ebook;
+import com.daniel.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
