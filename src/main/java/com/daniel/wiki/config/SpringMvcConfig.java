6009d65af01fb0888eb58d64f33f087ee11c0253
Xinwei Lu <490589474@qq.com>
2021-08-08 17:59:28 +0800

    5-8 增加Spring拦截器，打印接口耗时

---
+++ src/main/java/com/daniel/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.daniel.wiki.config;
+
+import com.daniel.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+    //配置过滤器
+    public void addInterceptors(InterceptorRegistry registry) {
+        //针对所有接口，除了login接口
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
